f8668681e7451b50496e47554b1428e93268e5ea
mayuri <mayurilugade2911@gmail.com>
2026-02-12 00:46:51 +0530

    first commit

---
+++ MultiTenantCRM.API/Controllers/PublicController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+
+namespace MultiTenantCRM.API.Controllers
+{
+    [Area("PublicArea")]
+    [DisplayName("Public Controller")]
+    [Route("api / [area]/[controlller]")]
+    public class PublicController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MultiTenantCRM.API/Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+c.SwaggerDoc("v1", new OpenApiInfo { Title = "CRM API", Version = "v1" })
+    ); 
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c=>c.SwaggerEndpoint("swagger/v1/swagger.json", "CRM API V1")
+        );
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+            name: "areas",
+            pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+          );
+
+app.Run();
